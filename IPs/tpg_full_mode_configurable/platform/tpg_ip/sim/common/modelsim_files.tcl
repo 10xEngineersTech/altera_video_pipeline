ea539f7393183d22b4c9787e961923537429ec82
Hamza Aziz <82494613+hamzaaziz-10xe@users.noreply.github.com>
2026-05-08 16:34:14 +0500

    Independent TPG IP with the output of AXIS Full mode, reconfigurable (#5)
    Co-authored-by: abdulraouf-10xe <abdul.raouf@10xengineers.ai>

---
+++ IPs/tpg_full_mode_configurable/platform/tpg_ip/sim/common/modelsim_files.tcl
@@ -0,0 +1,110 @@
+source [file join [file dirname [info script]] ./../../../ip/tpg_ip/tpg_ip_clock_in/sim/common/modelsim_files.tcl]
+source [file join [file dirname [info script]] ./../../../ip/tpg_ip/tpg_ip_reset_in/sim/common/modelsim_files.tcl]
+source [file join [file dirname [info script]] ./../../../ip/tpg_ip/tpg_ip_intel_vvp_tpg_0/sim/common/modelsim_files.tcl]
+
+namespace eval tpg_ip {
+  proc get_design_libraries {} {
+    set libraries [dict create]
+    set libraries [dict merge $libraries [tpg_ip_clock_in::get_design_libraries]]
+    set libraries [dict merge $libraries [tpg_ip_reset_in::get_design_libraries]]
+    set libraries [dict merge $libraries [tpg_ip_intel_vvp_tpg_0::get_design_libraries]]
+    dict set libraries altera_reset_controller_1924 1
+    dict set libraries tpg_ip                       1
+    return $libraries
+  }
+  
+  proc get_memory_files {QSYS_SIMDIR QUARTUS_INSTALL_DIR} {
+    set memory_files [list]
+    set memory_files [concat $memory_files [tpg_ip_clock_in::get_memory_files "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_clock_in/sim/" "$QUARTUS_INSTALL_DIR"]]
+    set memory_files [concat $memory_files [tpg_ip_reset_in::get_memory_files "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_reset_in/sim/" "$QUARTUS_INSTALL_DIR"]]
+    set memory_files [concat $memory_files [tpg_ip_intel_vvp_tpg_0::get_memory_files "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_intel_vvp_tpg_0/sim/" "$QUARTUS_INSTALL_DIR"]]
+    return $memory_files
+  }
+  
+  proc get_common_design_files {USER_DEFINED_COMPILE_OPTIONS USER_DEFINED_VERILOG_COMPILE_OPTIONS USER_DEFINED_VHDL_COMPILE_OPTIONS QSYS_SIMDIR} {
+    set design_files [dict create]
+    set design_files [dict merge $design_files [tpg_ip_clock_in::get_common_design_files $USER_DEFINED_COMPILE_OPTIONS $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_VHDL_COMPILE_OPTIONS "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_clock_in/sim/"]]
+    set design_files [dict merge $design_files [tpg_ip_reset_in::get_common_design_files $USER_DEFINED_COMPILE_OPTIONS $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_VHDL_COMPILE_OPTIONS "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_reset_in/sim/"]]
+    set design_files [dict merge $design_files [tpg_ip_intel_vvp_tpg_0::get_common_design_files $USER_DEFINED_COMPILE_OPTIONS $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_VHDL_COMPILE_OPTIONS "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_intel_vvp_tpg_0/sim/"]]
+    return $design_files
+  }
+  
+  proc get_design_files {USER_DEFINED_COMPILE_OPTIONS USER_DEFINED_VERILOG_COMPILE_OPTIONS USER_DEFINED_VHDL_COMPILE_OPTIONS QSYS_SIMDIR QUARTUS_INSTALL_DIR} {
+    set design_files [list]
+    set design_files [concat $design_files [tpg_ip_clock_in::get_design_files $USER_DEFINED_COMPILE_OPTIONS $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_VHDL_COMPILE_OPTIONS "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_clock_in/sim/" "$QUARTUS_INSTALL_DIR"]]
+    set design_files [concat $design_files [tpg_ip_reset_in::get_design_files $USER_DEFINED_COMPILE_OPTIONS $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_VHDL_COMPILE_OPTIONS "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_reset_in/sim/" "$QUARTUS_INSTALL_DIR"]]
+    set design_files [concat $design_files [tpg_ip_intel_vvp_tpg_0::get_design_files $USER_DEFINED_COMPILE_OPTIONS $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_VHDL_COMPILE_OPTIONS "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_intel_vvp_tpg_0/sim/" "$QUARTUS_INSTALL_DIR"]]
+    lappend design_files "vlog $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS  \"[normalize_path "$QSYS_SIMDIR/../altera_reset_controller_1924/sim/altera_reset_controller.v"]\"  -work altera_reset_controller_1924"  
+    lappend design_files "vlog $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS  \"[normalize_path "$QSYS_SIMDIR/../altera_reset_controller_1924/sim/altera_reset_synchronizer.v"]\"  -work altera_reset_controller_1924"
+    lappend design_files "vlog $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS  \"[normalize_path "$QSYS_SIMDIR/tpg_ip.v"]\"  -work tpg_ip"                                                                             
+    return $design_files
+  }
+  
+  proc get_non_duplicate_elab_option {ELAB_OPTIONS NEW_ELAB_OPTION} {
+    set IS_DUPLICATE [string first $NEW_ELAB_OPTION $ELAB_OPTIONS]
+    if {$IS_DUPLICATE == -1} {
+      return $NEW_ELAB_OPTION
+    } else {
+      return ""
+    }
+  }
+  
+  
+  proc get_elab_options {SIMULATOR_TOOL_BITNESS} {
+    set ELAB_OPTIONS ""
+    append ELAB_OPTIONS [get_non_duplicate_elab_option $ELAB_OPTIONS [tpg_ip_clock_in::get_elab_options $SIMULATOR_TOOL_BITNESS]]
+    append ELAB_OPTIONS [get_non_duplicate_elab_option $ELAB_OPTIONS [tpg_ip_reset_in::get_elab_options $SIMULATOR_TOOL_BITNESS]]
+    append ELAB_OPTIONS [get_non_duplicate_elab_option $ELAB_OPTIONS [tpg_ip_intel_vvp_tpg_0::get_elab_options $SIMULATOR_TOOL_BITNESS]]
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $ELAB_OPTIONS
+  }
+  
+  
+  proc get_sim_options {SIMULATOR_TOOL_BITNESS} {
+    set SIM_OPTIONS ""
+    append SIM_OPTIONS [tpg_ip_clock_in::get_sim_options $SIMULATOR_TOOL_BITNESS]
+    append SIM_OPTIONS [tpg_ip_reset_in::get_sim_options $SIMULATOR_TOOL_BITNESS]
+    append SIM_OPTIONS [tpg_ip_intel_vvp_tpg_0::get_sim_options $SIMULATOR_TOOL_BITNESS]
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $SIM_OPTIONS
+  }
+  
+  
+  proc get_env_variables {SIMULATOR_TOOL_BITNESS} {
+    set ENV_VARIABLES [dict create]
+    set LD_LIBRARY_PATH [dict create]
+    set LD_LIBRARY_PATH [dict merge $LD_LIBRARY_PATH [dict get [tpg_ip_clock_in::get_env_variables $SIMULATOR_TOOL_BITNESS] "LD_LIBRARY_PATH"]]
+    set LD_LIBRARY_PATH [dict merge $LD_LIBRARY_PATH [dict get [tpg_ip_reset_in::get_env_variables $SIMULATOR_TOOL_BITNESS] "LD_LIBRARY_PATH"]]
+    set LD_LIBRARY_PATH [dict merge $LD_LIBRARY_PATH [dict get [tpg_ip_intel_vvp_tpg_0::get_env_variables $SIMULATOR_TOOL_BITNESS] "LD_LIBRARY_PATH"]]
+    dict set ENV_VARIABLES "LD_LIBRARY_PATH" $LD_LIBRARY_PATH
+    if ![ string match "bit_64" $SIMULATOR_TOOL_BITNESS ] {
+    } else {
+    }
+    return $ENV_VARIABLES
+  }
+  
+  
+  proc normalize_path {FILEPATH} {
+      if {[catch { package require fileutil } err]} { 
+          return $FILEPATH 
+      } 
+      set path [fileutil::lexnormalize [file join [pwd] $FILEPATH]]  
+      if {[file pathtype $FILEPATH] eq "relative"} { 
+          set path [fileutil::relative [pwd] $path] 
+      } 
+      return $path 
+  } 
+  proc get_dpi_libraries {QSYS_SIMDIR} {
+    set libraries [dict create]
+    set libraries [dict merge $libraries [tpg_ip_clock_in::get_dpi_libraries "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_clock_in/sim/"]]
+    set libraries [dict merge $libraries [tpg_ip_reset_in::get_dpi_libraries "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_reset_in/sim/"]]
+    set libraries [dict merge $libraries [tpg_ip_intel_vvp_tpg_0::get_dpi_libraries "$QSYS_SIMDIR/../../ip/tpg_ip/tpg_ip_intel_vvp_tpg_0/sim/"]]
+    
+    return $libraries
+  }
+  
+}
---
+++ IPs/tpg_full_mode_configurable/platform/tpg_ip/tpg_ip_inst.v
@@ -0,0 +1,18 @@
+	tpg_ip u0 (
+		.clk_clk                                           (_connected_to_clk_clk_),                                           //   input,   width = 1,                                 clk.clk
+		.intel_vvp_tpg_0_axi4s_vid_out_tdata               (_connected_to_intel_vvp_tpg_0_axi4s_vid_out_tdata_),               //  output,  width = 24,       intel_vvp_tpg_0_axi4s_vid_out.tdata
+		.intel_vvp_tpg_0_axi4s_vid_out_tvalid              (_connected_to_intel_vvp_tpg_0_axi4s_vid_out_tvalid_),              //  output,   width = 1,                                    .tvalid
+		.intel_vvp_tpg_0_axi4s_vid_out_tready              (_connected_to_intel_vvp_tpg_0_axi4s_vid_out_tready_),              //   input,   width = 1,                                    .tready
+		.intel_vvp_tpg_0_axi4s_vid_out_tlast               (_connected_to_intel_vvp_tpg_0_axi4s_vid_out_tlast_),               //  output,   width = 1,                                    .tlast
+		.intel_vvp_tpg_0_axi4s_vid_out_tuser               (_connected_to_intel_vvp_tpg_0_axi4s_vid_out_tuser_),               //  output,   width = 3,                                    .tuser
+		.intel_vvp_tpg_0_av_mm_control_agent_address       (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_address_),       //   input,   width = 7, intel_vvp_tpg_0_av_mm_control_agent.address
+		.intel_vvp_tpg_0_av_mm_control_agent_write         (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_write_),         //   input,   width = 1,                                    .write
+		.intel_vvp_tpg_0_av_mm_control_agent_byteenable    (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_byteenable_),    //   input,   width = 4,                                    .byteenable
+		.intel_vvp_tpg_0_av_mm_control_agent_writedata     (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_writedata_),     //   input,  width = 32,                                    .writedata
+		.intel_vvp_tpg_0_av_mm_control_agent_read          (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_read_),          //   input,   width = 1,                                    .read
+		.intel_vvp_tpg_0_av_mm_control_agent_readdata      (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_readdata_),      //  output,  width = 32,                                    .readdata
+		.intel_vvp_tpg_0_av_mm_control_agent_readdatavalid (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_readdatavalid_), //  output,   width = 1,                                    .readdatavalid
+		.intel_vvp_tpg_0_av_mm_control_agent_waitrequest   (_connected_to_intel_vvp_tpg_0_av_mm_control_agent_waitrequest_),   //  output,   width = 1,                                    .waitrequest
+		.reset_reset                                       (_connected_to_reset_reset_)                                        //   input,   width = 1,                               reset.reset
+	);
+
